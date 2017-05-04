17703b25ec088a862c71926efdf436df9a7d4648
micos7 <micos7@gmail.com>
2017-05-04 15:12:49 +0300

    removed defaults

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center">
+<p> &copy; {{date('Y')}} &middot; Laracarte - a Laramap clone.</p>
+</footer>
---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,37 @@
+
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Laracarte</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Artisans</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Planet <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="https://laravel.com">Laravel.com</a></li>
+                <li><a href="https://laravel.iocom">Laravel.io</a></li>
+                <li><a href="https://laracasts.com">Laracasts</a></li>
+                <li><a href="https://larajobs.com">Larajobs</a></li>
+                <li><a href="https://laravel-news.com">Laravel news</a></li>
+                <li><a href="https://larachat.co">Larachat</a></li>
+              </ul>
+            </li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Login</a></li>
+              <li><a href="#">Register</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
