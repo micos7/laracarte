519aefb95b6882a8233338f199d1900f3f430598
micos7 <micos7@gmail.com>
2017-05-05 14:37:14 +0300

    active class current route

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -12,8 +12,8 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li><a href="{{route('root')}}">Home</a></li>
-            <li><a href=" {{route('about')}} ">About</a></li>
+            <li class=" {{set_active_route('root')}}"><a href="{{route('root')}}">Home</a></li>
+            <li class=" {{set_active_route('about')}}"><a href=" {{route('about')}} ">About</a></li>
             <li><a href="#contact">Artisans</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Planet <span class="caret"></span></a>
