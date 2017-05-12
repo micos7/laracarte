761222c547077e708fd03e30e1ddaf698c3ab9fd
micos7 <micos7@gmail.com>
2017-05-12 16:15:30 +0300

    send email

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
+use App\Mail\ContactMessageCreated;
+use Illuminate\Support\Facades\Mail;
 
 
 class ContactsController extends Controller
@@ -13,6 +15,9 @@ class ContactsController extends Controller
     }
 
     public function store(ContactRequest $request){
-    
+        $mailable = new ContactMessageCreated($request->name,$request->email,$request->msg);
+        Mail::to('admin@admin.ro')->send($mailable);
+
+        return 'Done!';
     }
 }
